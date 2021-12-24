24e2df7250161e1adc574dfac11b2889bbf7dab1
Minsoo Kim <minsookim@Minsoos-MacBook-Pro.local>
2021-12-24 10:51:02 -0500

    basic template based on ytvid

---
+++ readme.md
@@ -0,0 +1,4 @@
+# starting template
+https://www.youtube.com/watch?v=p61lhOvQg2Q&t=337s
+# run
+$ yarn start
---
+++ src/cljs/app/core.cljs
@@ -0,0 +1,16 @@
+(ns app.core
+  (:require [helix.core :refer [defnc $]]
+            [helix.dom :as d]
+            ["react-dom" :as rdom]))
+
+(defnc app []
+  (d/div
+   (d/h1 "Hello World!")))
+
+(defn render []
+  (rdom/render
+   ($ app)
+   (js/document.getElementById "root")))
+
+(defn ^:export init []
+  (render))
