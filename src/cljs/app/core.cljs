2df44b18797907455f604b93d591b85a4eaa75c9
Minsoo Kim <minsookim@Minsoos-MacBook-Pro.local>
2022-01-10 01:03:47 -0500

    polished items on bars

--- src/cljs/app/core.cljs
+++ src/cljs/app/core.cljs
@@ -1,57 +1,76 @@
 (ns app.core
   (:require [reagent.core :as r]
-            [reagent.dom :as rdom]))
+            [reagent.dom :as rdom]
+            ["halfmoon" :as moon]))
+
+(defonce state-db (r/atom {}))
+
+(defn toggle-sidebar-btn []
+  [:button {:class "btn btn-action"
+            :type "button"
+            :on-click (fn [] (moon/toggleSidebar))}
+   [:i {:class "fa fa-bars"
+        :aria-hidden "true"}]])
+
+(defn brand-logo []
+  [:a {:href "#"
+       :class "navbar-text text-monospace"}
+   [:img {:src "...", :alt "atomic"}]])
+
+(defn change-color-btn []
+  [:button {:class "btn btn-action"
+            :on-click
+            (fn []
+              (do (swap! state-db
+                         #(assoc % :dark-mode
+                                 (-> @state-db :dark-mode not)))
+                  (moon/toggleDarkMode)))}
+   [:i {:class "fas fa-moon"
+        :aria-hidden "true"}]])
+
+(defn search-box []
+  [:input {:type "text"
+           :class "form-control"
+           :placeholder "search"
+           :required "required"}])
+
+(defn sign-change-btn []
+  [:button {:class "btn btn-primary"
+            :type "submit"}
+   "Sign up"])
+
+(defn version-label []
+  [:span {:class "navbar-text text-monospace"}
+   "v0.1"])
+
+(defn service-labels []
+  [:ul {:class "navbar-nav d-none d-md-flex"} " "
+   [:li {:class "nav-item active"}
+    [:a {:href "#", :class "nav-link"}
+     "Docs"]]
+   [:li {:class "nav-item"}
+    [:a {:href "#", :class "nav-link"}
+     "Products"]]])
+
+(defn nav-content []
+  [:div {:class "navbar-content"}
+   (toggle-sidebar-btn)
+   (brand-logo)
+   (version-label)
+   (service-labels)])
+
+(defn nav-form []
+  [:form {:class "form-inline d-none d-md-flex ml-auto"}
+   (change-color-btn)
+   (search-box)
+   (sign-change-btn)])
 
 (defn navbar []
   [:div {:class "page-wrapper with-navbar"}
-   [:nav {:class "navbar"}
-    [:div {:class "navbar-content"}
-     [:button {:class "btn btn-action", :type "button"}
-      [:i {:class "fa fa-bars", :aria-hidden "true"}]
-      [:span {:class "sr-only"} "Toggle sidebar"]" "]]  
-    [:a {:href "#", :class "navbar-brand"}
-     [:img {:src "...", :alt "..."}]]  
-    [:span {:class "navbar-text text-monospace"} "v0.1"] " "  
-    [:ul {:class "navbar-nav d-none d-md-flex"} " "
-     [:li {:class "nav-item active"}
-      [:a {:href "#", :class "nav-link"} "Docs"]]
-     [:li {:class "nav-item"}
-      [:a {:href "#", :class "nav-link"} "Products"]]]
-    [:form {:class "form-inline d-none d-md-flex ml-auto"
-            :action "..."
-            :method "..."}  
-     [:input {:type "text"
-              :class "form-control"
-              :placeholder "Email address"
-              :required "required"}]
-     [:button {:class "btn btn-primary"
-               :type "submit"}
-      "Sign up"]]  
-    [:div {:class "navbar-content d-md-none ml-auto"} " "  
-     [:div {:class "dropdown with-arrow"}
-      [:button {:class "btn"
-                :data-toggle "dropdown"
-                :type "button"
-                :id "navbar-dropdown-toggle-btn-1"}
-       [:i {:class "fa fa-angle-down"
-            :aria-hidden "true"}]]
-      [:div {:class "dropdown-menu dropdown-menu-right w-200"
-             :aria-labelledby "navbar-dropdown-toggle-btn-1"} " "  
-       [:a {:href "#", :class "dropdown-item"} "Docs"]
-       [:a {:href "#", :class "dropdown-item"} "Products"]
-       [:div {:class "dropdown-divider"}]
-       [:div {:class "dropdown-content"}
-        [:form {:action "...", :method "..."}
-         [:div {:class "form-group"}
-          [:input {:type "text"
-                   :class "form-control"
-                   :placeholder "Email address"
-                   :required "required"}]]
-         [:button {:class "btn btn-primary btn-block"
-                   :type "submit"}
-          "Sign up"]]]]]]]  
-   [:div {:class "content-wrapper"}
-    "Futix"]])
+   [:nav
+    {:class "navbar"}
+    (nav-content)
+    (nav-form)]])
 
 (defn item-in-side-bar [icon subject]
   [:a {:href "#"
@@ -68,29 +87,25 @@
     [:h5 {:class "sidebar-title"}
      "Components"]
     [:div {:class "sidebar-divider"}]    
-    (item-in-side-bar "fa fa-terminal" "Dashboard")
+    (item-in-side-bar "fa fa-terminal" "Dashboard?")
     (item-in-side-bar "fa fa-terminal" "Journal")
     (item-in-side-bar "fa fa-camera-retro fa-lg" "Balance")]])
 
-#_(defn change-color-mode []
-  [:div
-   [:input {:type "button" :value "Click me!"
-            :on-click (fn [] halfmoon/toggleDarkMode)}]])
-
 (defn scaffold []
   [:div
-   [navbar]
-   [sidebar]
-  ;; [change-color-mode]
-   [:h2 "abcdef"]])
+   (navbar)
+   (sidebar)])
 
-(def id-to-html "root")
+(defonce id-to-html "root")
 
 (defn run []
-  (rdom/render [scaffold]
-               (js/document.getElementById id-to-html)))
+  (rdom/render
+   (scaffold)
+   (js/document.getElementById id-to-html)))
 
 (defn ^:export init []
+  (swap! state-db (fn [db] (assoc db :dark-mode true)))
+  (moon/toggleDarkMode)
   (run)
   (js/console.log "Loaded"))
 
