c7493dc7a2e4aefd76bec659a2fe271c24b0b1ed
Minsoo Kim <minsookim@Minsoos-MacBook-Pro.local>
2022-01-20 02:11:49 -0500

    connected db in and out

--- src/clj/server/core.clj
+++ src/clj/server/core.clj
@@ -1,7 +1,9 @@
 (ns server.core
   (:require
-   [clojure.set :refer [join]]
-   [clojure.string :refer [join] :rename {join strjoin}]
+   [clojure.set :refer [join]
+    :rename {join set-join}]
+   [clojure.string :refer [join]
+    :rename {join str-join}]
    [ring.adapter.jetty :refer [run-jetty]]
    [ring.middleware.json :refer
     [wrap-json-response wrap-json-body]]
@@ -13,76 +15,189 @@
     [routes GET POST PUT DELETE]]
    [compojure.route :refer [not-found]]
    [next.jdbc :as jdbc]
+   [next.jdbc.sql :as sql]
+   [next.jdbc.date-time :as time]
    ;;[integrant.core :refer [ref] :rename {ref iref}]
-   [shared.random :refer [uuid]]))
+   [shared.random :refer [uuid]]
+   [java-time :as jt]))
 
-(def db {:dbtype "postgres"
-         :dbname "pan"})
 
-(def ds (jdbc/get-datasource db))
+
+
+;; time
+
+(defn make-date-zoned [{:keys [year month day]}]
+  (jt/zoned-date-time year month day))
+
+
+(defn make-years [n]
+  (jt/years n))
+
+(defn make-months [n]
+  (jt/months n))
+
+(defn make-days [n]
+  (jt/days n))
+
+(defn make-date [{:keys [year month day]}]
+  (jt/local-date year month day))
+
+(jt/local-date-time 2021 4 12 13 23 31 123)
+
+
+(defn sqldate->localdate [sqldate]
+  (jt/local-date sqldate))
+
+(defn make-stamp []
+  (jt/instant->sql-timestamp 
+   (jt/instant)))
+
+(defn str->date [s]
+  (jt/local-date "yyyy-MM-dd" s))
+
+(defn date->str [d]
+  (jt/format "yyyy-MM-dd" d))
+
+(defn ensure-date-type [x]
+  (if (string? x)
+    (str->date x)
+    x))
+
+(defn ensure-date-string [x]
+  (if (string? x)
+    x (date->str x)
+    ))
+
+(defn add [{:keys [date span]}]
+  (jt/plus date span))
+
+(defn subtract [{:keys [date span]}]
+  (jt/minus date span))
+
+(defn until [{:keys [earlier later]}]
+  (let [ens-earlier (ensure-date-type earlier)
+        ens-later (ensure-date-type later)]
+    (.until ens-earlier ens-later (java.time.temporal.ChronoUnit/DAYS))))
+
+(defn later? [earlier later]
+  (> (until {:earlier earlier :later later}) 0))
+
+;;;; Db
+
+(def db-info
+  {:dbtype "postgres"
+   :dbname "pan"
+   :host "localhost"})
+
+(def db-conn
+  (jdbc/get-datasource db-info))
+
+(defn convert-symbol [symb]
+  (case (name symb)
+    "p-open" "("
+    "p-close" ")"
+    "comma" ","
+    "semicolon" ";"
+    symb))
 
 (defn sqlize [vec]
-(->> vec
-     (map name)
-     (map (fn [s] (case s
-                    "p-open" "("
-                    "p-close" ")"
-                    "comma" ","
-                    s)))
-     (strjoin " ")))
+  (->> vec
+       (map convert-symbol)
+       (str-join " ")))
 
-(def migrations
-  {:up
-   {:client 
-    (sqlize ['create 'table 'client 'p-open
-             'id 'serial 'primary 'key 'comma
-             'name 'text 'not 'null 'p-close])}
-   {:person
-    (sqlize ['create 'table 'if 'not 'exists 'person
-             'p-open 'id 'serial 'primary 'key
-             'comma 'name 'text 'not 'null 'p-close])}
-   :down
-   {:client 
-    (sqlize ['drop 'table 'if 'exists 'client])}
-   {:person
-    (sqlize ['drop 'table 'if 'exists 'person])}})
 
-(jdbc/execute! ds [(:client-up migrations)])
+;;;; migration
+
+(def mig-up-sql
+  {:client 
+   (sqlize ['create 'table 'if 'not 'exists 'client
+            'p-open 'aid 'serial 'primary 'key
+            'comma 'eid 'uuid 'not 'null
+            'comma 'v_from 'timestamp 'not 'null
+            'comma 'v_thru 'timestamp 'not 'null
+            'comma 'legal_name 'text 'not 'null
+            'comma 'short_name 'text 'not 'null
+            'comma 'num_person 'int 'not 'null
+            'comma 'num_manager 'int 'not 'null
+            'p-close 'semicolon])
+   :person
+   (sqlize ['create 'table 'if 'not 'exists 'person
+            'p-open 'aid 'serial 'primary 'key
+            'comma 'eid 'uuid 'not 'null
+            'comma 'v_from 'timestamp 'not 'null
+            'comma 'v_thru 'timestamp 'not 'null
+            'comma 'client_id 'uuid 'not 'null
+            'comma 'first_name 'text 'not 'null
+            'comma 'last_name 'text 'not 'null
+            'comma 'is_manager 'boolean 'not 'null
+            'p-close 'semicolon])})
+
+(def mig-down-sql
+  {:client
+   (sqlize ['drop 'table 'if 'exists 'client 'semicolon])
+   :person
+   (sqlize ['drop 'table 'if 'exists 'person 'semicolon])})
+
+(defn db-up []
+  (doseq [sql (vals mig-up-sql)]
+    (jdbc/execute! db-conn [sql])))
+
+(defn db-down []
+  (doseq [sql (vals mig-down-sql)]
+    (jdbc/execute! db-conn [sql])))
+
+;; (db-up)
+;; (db-down)
 
 ;;;;;;;;;;
 
 (def clients
-  #{{:_id "882b6098-0ac6-45cb-9722-dd0e120a5f9d"
-     :eid "d09eb3a2-217f-4f93-80a3-22672fb7945b"
-     :v_from "11/1/2020"
-     :v_thru "12/2/2021"
+  #{{:eid #uuid "ac197719-1f37-4c2d-a689-23872896991b"
+     :v_from (make-stamp)
+     :v_thru (make-stamp)
      :legal_name "Bitem, LLC"
-     :short_name "Bitem"}})
+     :short_name "Bitem"
+     :num_person 2
+     :num_manager 1}})
 
 (def persons
-  #{{:_id "2af229dc-e45a-4bdb-a15f-786b7b7b70b5"
-     :eid "ac197719-1f37-4c2d-a689-23872896991b"
-     :v_from "11/1/2020"
-     :v_thru "12/2/2021"
-     :client_eid "d09eb3a2-217f-4f93-80a3-22672fb7945b"
+[{:eid   #uuid "1902c205-2bc6-40b8-943b-f5b199241316" 
+     :v_from (make-stamp);; => #inst "2022-01-20T06:59:31.627826000-00:00"
+     :v_thru #inst "5000-01-20T06:59:31.627826000-00:00"
+     :client_id #uuid "ac197719-1f37-4c2d-a689-23872896991b"
      :first_name "Darren"
-     :last_name "Kim"}
-    {:_id "842dbf58-21f6-4cf9-b04b-4d3a88630a18"
-     :eid "3ae28020-0f99-4a95-ab0d-37411a438f35"
-     :v_from "11/1/2020"
-     :v_thru "12/2/2021"
-     :client_eid "d09eb3a2-217f-4f93-80a3-22672fb7945b"
+     :last_name "Kim"
+     :is_manager true}
+    {:eid #uuid "3ae28020-0f99-4a95-ab0d-37411a438f35"
+     :v_from (make-stamp)
+     :v_thru #inst "5000-01-20T06:59:31.627826000-00:00"
+     :client_id #uuid "ac197719-1f37-4c2d-a689-23872896991b"
      :first_name "Jack"
-     :last_name "Reacher"}})
+     :last_name "Reacher"
+     :is_manager false}])
 
-(join persons clients {:client_eid :eid})
+(sql/insert! db-conn :client (first clients))
+
+(sql/insert! db-conn :person (first persons))
+(sql/insert! db-conn :person (second persons))
+
+
+(defn get-clients []
+  (jdbc/execute! db-conn ["select * from client;"]))
+
+(defn get-persons []
+  (jdbc/execute! db-conn ["select * from person;"]))
+
+
+
+;; (set-join persons clients {:client_eid :eid})
 
 (def paths
   (routes
    (GET "/" [] (response "asdfdf"))
    (GET "/test" [] (response {:baz "qsssux"}))
-   (GET "/api/clients" [] (response clients))
-   (GET "/api/persons" [] (response persons))
+   (GET "/api/clients" [] (response (get-clients)))
+   (GET "/api/persons" [] (response (get-persons)))
    (not-found {:error "Not found"})))
 
 (def app
